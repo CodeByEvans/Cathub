a7d3522f8b1562afe0a3343cf98697c2e64579cd
CodeByEvans <evansjavierlr@gmail.com>
2026-02-19 15:45:07 +0100

    Fix/Backend: Index.js fixed

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando");
+  res.send("Aquí está el backend de Cathub :)");
 });
-
-app;
 
 app.get("/api/accept-connection/:request_id", (req, res) => {
   const requestId = req.params.request_id;
