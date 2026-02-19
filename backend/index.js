9575b66774cdf9c044aa05388336a5bf70722506
CodeByEvans <evansjavierlr@gmail.com>
2026-02-19 15:47:57 +0100

    feat/backend: Imported Dynamic Port to index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,8 @@ app.get("/api/accept-connection/:request_id", (req, res) => {
   res.redirect(deepLink);
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Servidor en http://localhost:3000");
 });
