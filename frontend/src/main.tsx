5c9dae5939bcaaed0bf2297ad3bcb06e2cc24a9a
CodeByEvans <evansjavierlr@gmail.com>
2026-02-23 17:36:24 +0100

    feat: Add initialize of themeService for better UX

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,22 @@ import App from "./App";
 import AuthGuard from "./modules/auth/components/AuthGuard";
 import { Toaster } from "./components/ui/sonner";
 import DeepLinkListener from "./modules/deep-link/DeepLinkListener";
+import { themeService } from "./services/theme.service";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <DeepLinkListener />
-    <Toaster />
-    <AuthGuard>
-      <App />
-    </AuthGuard>
-  </React.StrictMode>,
-);
+async function bootstrap() {
+  // 1️⃣ Espera a que se cargue el theme
+  await themeService.getTheme(); // Esto ya aplica la clase en document.documentElement
+
+  // 2️⃣ Ahora renderiza React
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <DeepLinkListener />
+      <Toaster />
+      <AuthGuard>
+        <App />
+      </AuthGuard>
+    </React.StrictMode>,
+  );
+}
+
+bootstrap();
