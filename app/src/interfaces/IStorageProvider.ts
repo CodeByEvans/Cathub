a037bef93134ff37b4ba81e3c3478c24afe38600
CodeByEvans <evansjavierlr@gmail.com>
2026-04-06 23:44:20 +0200

    feat(storage): add new actions

--- app/src/interfaces/IStorageProvider.ts
+++ app/src/interfaces/IStorageProvider.ts
@@ -1,4 +1,8 @@
+import { StoreKey } from "@/shared/infrastructure/store.keys";
+
 export interface IStorageProvider {
-  get(key: string): Promise<string | null>;
-  set(key: string, value: string): Promise<void>;
+  get(key: StoreKey): Promise<string | null>;
+  set(key: StoreKey, value: string): Promise<void>;
+  delete(key: StoreKey): Promise<void>;
+  clear(keys: StoreKey[]): Promise<void>;
 }
