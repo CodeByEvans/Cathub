187bacd8d4f4d9a6371260cf059abe6714173bac
CodeByEvans <evansjavierlr@gmail.com>
2026-04-06 23:45:24 +0200

    refactor: add InnerProviders to conditionally wrap contexts when linked

--- app/src/providers/AppProdivers.tsx
+++ app/src/providers/AppProdivers.tsx
@@ -1,12 +1,29 @@
 import { CallProvider } from "@/modules/call/context/CallContext";
+import { ConnectionProvider } from "@/modules/connection/contexts/ConnectionContext";
 import { PresenceProvider } from "@/modules/presence/context/PresenceContext";
+import { useConnection } from "@/modules/connection/contexts/ConnectionContext";
+import { NotesProvider } from "@/modules/notes/context/NotesContext";
 
-export const AppProviders: React.FC<{ children: React.ReactNode }> = ({
+const InnerProviders: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const { isLinked } = useConnection();
+
+  if (!isLinked) return <>{children}</>;
+
   return (
     <CallProvider>
-      <PresenceProvider>{children}</PresenceProvider>
+      <PresenceProvider>
+        <NotesProvider>{children}</NotesProvider>
+      </PresenceProvider>
     </CallProvider>
   );
 };
+
+export const AppProviders: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <ConnectionProvider>
+    <InnerProviders>{children}</InnerProviders>
+  </ConnectionProvider>
+);
