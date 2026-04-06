df9d6c6dc88741b9ad97af49c8a491cc298e83f9
CodeByEvans <evansjavierlr@gmail.com>
2026-04-06 23:35:08 +0200

    feat: add api url

--- app/src/config/envs.ts
+++ app/src/config/envs.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   VITE_SUPABASE_URL: z.string().url(),
   VITE_SUPABASE_ANON_KEY: z.string().min(1),
   VITE_OPEN_WEATHER_API_KEY: z.string().min(1),
+  API_URL: z.string().optional(),
 });
 
 const envVars = {
@@ -11,6 +12,7 @@ const envVars = {
   VITE_SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90dnVkcHJrc2xrcmlsZmttYWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTI2MDcsImV4cCI6MjA3NzcyODYwN30.gzkW1xId_wb6UwKco3s2OXdvWlriB0gqGmuWbLXZ-fg",
   VITE_OPEN_WEATHER_API_KEY: "For future",
+  API_URL: "https://cathub-backend.vercel.app",
 };
 
 const parsed = envSchema.parse(envVars);
@@ -19,4 +21,5 @@ export const envs = {
   supabaseUrl: parsed.VITE_SUPABASE_URL,
   supabaseAnonKey: parsed.VITE_SUPABASE_ANON_KEY,
   openWeatherApiKey: parsed.VITE_OPEN_WEATHER_API_KEY,
+  apiUrl: parsed.API_URL,
 };
